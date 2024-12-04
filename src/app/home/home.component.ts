70cfe4319412fb22382c234ad2e7a33dcefee815
ThanaboonChantasawat <thanaboon.chan@bumail.net>
2024-12-05 00:10:05 +0700

    hotfix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private navbarCollapse?: bootstrap.Collapse;
+
+  ngOnInit() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarContent = document.querySelector('#navbarNav');
+    
+    if (navbarToggler && navbarContent) {
+      navbarToggler.addEventListener('click', () => {
+        this.navbarCollapse = new bootstrap.Collapse(navbarContent);
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.navbarCollapse?.dispose();
+  }
+}
